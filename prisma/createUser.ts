360db207940cf1c844fdf80db70b0068b47e00d7
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2025-12-04 10:39:39 +0700

    turn off proxy

--- prisma/createUser.ts
+++ prisma/createUser.ts
@@ -1,15 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 async function main() {
   // Hash Password
-  const hashedPassword = await bcrypt.hash("GwehKeren123!", 10); // ganti password
+  const hashedPassword = await bcrypt.hash("Pekanbaru123!", 10); // ganti password
   console.log("🙍‍♂️ Mebuat user baru...");
   await prisma.user.create({
     data: {
-      email: "muhammadeageltri@gmail.com", // ganti email
-      fullname: "Muhammad Eagel Triutama", // ganti nama
+      email: "wagianto190617@gmail.com", // ganti email
+      fullname: "Wagianto", // ganti nama
       password: hashedPassword,
+      role: Role.SUPERADMIN
     },
   });
   console.log("✅ Membuat user baru selesai.");
